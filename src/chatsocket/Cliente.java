84d4ef159cc93a543beadcfe5d3e446051396b8b
leocassiosilva <leocassiosilva1234@gmail.com>
2020-10-22 12:02:12 -0300

    costruindo cliente

--- src/chatsocket/Cliente.java
+++ src/chatsocket/Cliente.java
@@ -6,7 +6,9 @@
 package chatsocket;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,10 +17,20 @@ import java.util.logging.Logger;
  * @author Leocassio
  */
 public class Cliente {
+
     public static void main(String[] args) {
         try {
-             Socket conexao = new Socket("localhost", 999); //estabelecer a conexao
+            Socket conexao = new Socket("localhost", 999); //estabelecer a conexao
             System.out.println("Conectado!");
+
+            PrintStream saida = new PrintStream(conexao.getOutputStream());
+
+            Scanner teclado = new Scanner(System.in);
+             String mensagem;
+            while ((mensagem = teclado.nextLine()) != null) {
+                saida.println(teclado.nextLine());
+            }
+
         } catch (IOException ex) {
             Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
         }
